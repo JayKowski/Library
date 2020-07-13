abf9d1d6c0d1cfd8cf60f7ae91da62fccdfdefd1
JayKowski <jaymaina5@gmail.org>
2020-07-13 23:58:03 +0300

    grabbed user input, re-rendered it to the DOM

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 
-function Book(title, author, numberOfPages, read){
+function Book(title, author, pages, read){
     this.title = title,
     this.author = author,
-    this.numberOfPages = numberOfPages,
+    this.pages = pages,
     this.read = read
 };
 
@@ -21,16 +21,52 @@ document.getElementById('book-list').innerHTML = myLibrary.map(book =>
     `<div>
       <div>Title: ${book.title}</div>
       <div>Author: ${book.author}</div>
-    </div>`
+      <div>Pages: ${book.pages}</div>
+      <div>Read: ${book.read}</div>
+    </div><br>`
 ).join('')
 
 
-$("#addform").submit(function (e) {
+// $("#addform").submit(function (e) {
+//     e.preventDefault();
+//     let book = new Book    ($("input[name = 'name']").val(),
+//                             $("input[name = 'author']").val(),
+//                             $("input[name = 'pages']").val(),
+//                             $("input[name = 'read']").val());
+//     list.push(book);
+//     console.log(list);
+// });
+
+function addBookToDOM(){
+    let list = document.querySelector('#book-list');
+    let title = document.createElement('p');
+    let author = document.createElement('p');
+    let pages = document.createElement('p')
+    let read = document.createElement('p')
+}
+
+const form = document.querySelector('#new-book');
+
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    let book = new Book    ($("input[name = 'name']").val(),
-                            $("input[name = 'author']").val(),
-                            $("input[name = 'pages']").val(),
-                            $("input[name = 'read']").val());
-    list.push(book);
-    console.log(list);
-});
+    const name = form.querySelector('input[name="name"]').value;
+    const author = form.querySelector('input[name="author"]').value;
+    const pages = form.querySelector('input[name="pages"]').value;
+    const read = form.querySelector('input[name="read"]').checked;
+
+    if (name && author && pages){
+        var book = new Book(name, author, pages, read)
+        addBookToLibrary(book)
+        document.getElementById('book-list').innerHTML = myLibrary.map(book =>
+            `<div>
+                <div>Title: ${book.title}</div>
+                <div>Author: ${book.author}</div>
+                <div>Pages: ${book.pages}</div>
+                <div>Read: ${book.read}</div>
+            </div><br>`
+        ).join('')
+        console.log("success, all fields available")
+    } else {
+        console.log("not all fields are checked")
+    }
+})
