c0b681ff8c9a29aae5a95f3270042d0f8f957a1e
A L V A R O <alvarop3000@gmail.com>
2020-07-13 16:51:51 -0500

    update

--- index.js
+++ index.js
@@ -11,40 +11,6 @@ function addBookToLibrary(...args){
     myLibrary.push(...args);
 };
 
-const bookOne = new Book("a thousand ways to die", "Sony Entertainment");
-const bookTwo  = new Book("Robinson Crusoe", "Dont know the author");
-const bookThree = new Book("Leaders of Tomorrow", "the comedian");
-
-addBookToLibrary(bookOne, bookTwo, bookThree);
-
-document.getElementById('book-list').innerHTML = myLibrary.map(book => 
-    `<div>
-      <div>Title: ${book.title}</div>
-      <div>Author: ${book.author}</div>
-      <div>Pages: ${book.pages}</div>
-      <div>Read: ${book.read}</div>
-    </div><br>`
-).join('')
-
-
-// $("#addform").submit(function (e) {
-//     e.preventDefault();
-//     let book = new Book    ($("input[name = 'name']").val(),
-//                             $("input[name = 'author']").val(),
-//                             $("input[name = 'pages']").val(),
-//                             $("input[name = 'read']").val());
-//     list.push(book);
-//     console.log(list);
-// });
-
-function addBookToDOM(){
-    let list = document.querySelector('#book-list');
-    let title = document.createElement('p');
-    let author = document.createElement('p');
-    let pages = document.createElement('p')
-    let read = document.createElement('p')
-}
-
 const form = document.querySelector('#new-book');
 
 form.addEventListener('submit', function(e){
@@ -53,6 +19,10 @@ form.addEventListener('submit', function(e){
     const author = form.querySelector('input[name="author"]').value;
     const pages = form.querySelector('input[name="pages"]').value;
     const read = form.querySelector('input[name="read"]').checked;
+    btn.addEventListener("click", function () {
+        div.removeChild(document.getElementById(btn.id));
+        div.removeChild(btn);
+    });
 
     if (name && author && pages){
         var book = new Book(name, author, pages, read)
