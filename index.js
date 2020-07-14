8281e84ae38bfcde4142478022e349c93891c1fc
A L V A R O <alvarop3000@gmail.com>
2020-07-14 11:50:38 -0500

    create delete button

--- index.js
+++ index.js
@@ -2,43 +2,49 @@ let myLibrary = [];
 
 function Book(title, author, pages, read) {
     this.title = title,
-        this.author = author,
-        this.pages = pages,
-        this.read = read
+    this.author = author,
+    this.pages = pages,
+    this.read = read
 };
 
 function addBookToLibrary(...args) {
     myLibrary.push(...args);
 };
 
-const bookOne = new Book("a thousand ways to die", "Sony Entertainment", 234, true);
-const bookTwo = new Book("Robinson Crusoe", "Dont know the author", 234, true);
-const bookThree = new Book("Leaders of Tomorrow", "the comedian", 234, true);
+const bookOne = new Book("Test 1", "Joe", 234, true);
+const bookTwo = new Book("Test 2", "Sam", 234, true);
+const bookThree = new Book("Test 3", "Leo", 234, true);
 
 addBookToLibrary(bookOne, bookTwo, bookThree);
-
-const bookList = document.querySelector("#book-list")
-
 
 myLibrary.forEach(function (book) {
     const bookList = document.getElementById('book-list')
 
     let ul = document.createElement('UL')
+    ul.classList.add('book-details')
 
     let li1 = document.createElement('LI')
+    li1.classList.add('book-name')
     let li2 = document.createElement('LI')
+    li2.classList.add('book-author')
     let li3 = document.createElement('LI')
+    li3.classList.add('book-pages')
     let li4 = document.createElement('LI')
+    li4.classList.add('book-read')
+    let deleteButton = document.createElement('BUTTON')
+    deleteButton.classList.add('delete-btn')
 
     li1.innerText = `Title: ${book.title}`
     li2.innerText = `Author: ${book.author}`
     li3.innerText = `Pages: ${book.pages}`
     li4.innerText = `Read: ${book.read}`
+    deleteButton.innerText = `Delete Book`
 
     ul.appendChild(li1)
     ul.appendChild(li2)
     ul.appendChild(li3)
     ul.appendChild(li4)
+    ul.appendChild(deleteButton)
 
     bookList.appendChild(ul)
 
@@ -59,28 +65,48 @@ form.addEventListener('submit', function (e) {
         const bookList = document.getElementById('book-list')
 
         let ul = document.createElement('UL')
+        ul.classList.add('book-details')
 
         let li1 = document.createElement('LI')
+        li1.classList.add('book-name')
         let li2 = document.createElement('LI')
+        li2.classList.add('book-author')
         let li3 = document.createElement('LI')
+        li3.classList.add('book-pages')
         let li4 = document.createElement('LI')
+        li4.classList.add('book-read')
+        let deleteButton = document.createElement('BUTTON')
+        deleteButton.classList.add('delete-btn')
 
-        li1.innerText = `Title: ${book.title}`
-        li2.innerText = `Author: ${book.author}`
-        li3.innerText = `Pages: ${book.pages}`
-        li4.innerText = `Read: ${book.read}`
+        li1.innerText = `Title: ${name}`
+        li2.innerText = `Author: ${author}`
+        li3.innerText = `Pages: ${pages}`
+        li4.innerText = `Read: ${read}`
+        deleteButton.innerText = `Delete Book`
+
 
         ul.appendChild(li1)
         ul.appendChild(li2)
         ul.appendChild(li3)
         ul.appendChild(li4)
+        ul.appendChild(deleteButton)
 
         bookList.appendChild(ul)
 
-        console.log("success, all fields available")
+        alert("success, all fields available")
     } else {
-        console.log("not all fields are checked")
+        alert("not all fields are checked")
     }
 
-
 });
+
+const deleteBook = document.querySelector('#book-list')
+
+console.log(deleteBook)
+
+deleteBook.addEventListener('click', (e) => {
+    if(e.target.className == 'delete-btn') {
+        const ul = e.target.parentElement;
+        ul.parentNode.removeChild(ul);
+    }
+})
